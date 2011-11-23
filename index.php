7470fff0d3d9545560b9f7b163180b4db512a6e0
Toon Borgers <borgers.toon@gmail.com>
2011-11-23 11:12:21 +0100

    Basis-css aangemaakt

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Chiro Kasterlee</title>
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+	</head>
+	
+	</body>
+		<div id="center">
+			<div id="head">
+			
+			</div>
+			<div id="menu">
+				<?php include_once "menu.php" ?>
+			</div>
+			<div id="content">
+			</div>
+		</div>
+	</body>
+</html>
