06185df6f04840c1df4a2aea12419557dac01b9d
Toon Borgers <borgers.toon@gmail.com>
2011-11-24 20:04:56 +0100

    db-functionaliteit toegevoegd + eerste versie nieuwsberichten

---
+++ db.php
@@ -0,0 +1,49 @@
+<?php
+
+	class DB{
+		const USER="toon";
+		const PASSWORD="toon";
+		const SERVER="localhost";
+		const DATABASE="chiro";
+		private $link;
+		
+		private function construct__(){
+			
+		}
+				
+		private function destruct__ (){
+			disconnect();
+			unset ( $this );
+		}
+		
+		private function connect(){
+			$this->link = mysql_connect(self::SERVER,self::USER,self::PASSWORD);
+			if (!$this->link) {
+				die('Could not connect: ' . mysql_error());
+			}
+						
+			$db_selected = mysql_select_db(self::DATABASE, $this->link);
+			if (!$db_selected) {
+				die ('Could not connect: ' . mysql_error());
+			}
+		}
+		
+		private function disconnect(){
+			mysql_close($this->link);			
+		}
+		
+		public function doeQuery($query){
+			$this->connect();
+			$result = mysql_query($query, $this->link);
+			$resultaten = array();
+			
+			if($result){			
+				while($row = mysql_fetch_assoc($result)){
+					array_push($resultaten, $row);
+				}
+				$this->disconnect();
+			}
+			return $resultaten;
+		}
+	}
+?>
---
+++ home.php
@@ -0,0 +1,14 @@
+<h2>Nieuws</h2>
+<?php
+	include_once 'db.php';
+	$db = new DB();
+	$nieuwsberichten = $db->doeQuery('SELECT * FROM nieuws_item ORDER BY datum DESC');
+	foreach($nieuwsberichten as $nieuwsbericht){
+		echo "<div class='nieuwsbericht'>";
+		echo $nieuwsbericht['tekst'];
+		echo "<div class='nieuwsberichtDatum'>";		
+		echo $nieuwsbericht['datum'];
+		echo "</div>";
+		echo "</div>";
+	}
+?>
