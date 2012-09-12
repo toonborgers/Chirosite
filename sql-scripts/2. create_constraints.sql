63331fba264a3038522e677c0f5d83b64dfb5b4f
Toon Borgers <borgers.toon@gmail.com>
2012-09-12 21:15:04 +0200

    toon: oude dingen in old.zip gezet

--- sql-scripts/2. create_constraints.sql
+++
@@ -1,7 +0,0 @@
-alter table leiding_rol ADD CONSTRAINT fk_1 FOREIGN KEY (leiding_id) REFERENCES leiding (id);
-alter table leiding_rol ADD CONSTRAINT fk_2 FOREIGN KEY (rol_id) REFERENCES rol (id);
-
-alter table leiding_groep ADD CONSTRAINT fk_3 FOREIGN KEY (leiding_id) REFERENCES leiding (id);
-alter table leiding_groep ADD CONSTRAINT fk_4 FOREIGN KEY (groep_id) REFERENCES groep (id);
-
-alter table programma add constraint fk_5 foreign key (groep_id) REFERENCES groep (id);
