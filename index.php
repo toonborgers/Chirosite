c74b863618764e78c40fecbb39a7e6583413073d
Roger Vandewalle <stijn_hoskens2@hotmail.com>
2012-09-17 14:26:13 +0200

    een ideeke voor 't design

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="author" content="Wink Hosting (www.winkhosting.com)" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link rel="stylesheet" href="images/style.css" type="text/css" />
+	<title>chiro Kasterlee</title>
+</head>
+<body>
+	<div id="page" align="center">
+		<div id="content" style="width:800px">
+			<div id="logo"></div>
+			<div id="topheader">
+				<div align="left" class="bodytext">
+					<br />
+					<strong>Jeetskenteetsken</strong><br />
+					Hier kunde nog wa shit zette as ge wilt
+				</div>
+			</div>
+			<div id="menu">
+				<div align="right" class="smallwhitetext" style="padding:9px;">
+					<a href="#">Home</a> | <a href="#">Jongens</a> | <a href="#">Meisjes</a> | <a href="#">Foto's</a> | <a href="#">Chirofeesten</a> | <a href="#">Contacteer ons</a>
+				</div>
+			</div>
+			<div id="submenu">
+				<div align="right" class="smallgraytext" style="padding:9px;">
+					<a href="#">Info</a> | <a href="#">Programma's</a> | <a href="#">Het kamp</a> | <a href="#">Kalender</a> | <a href="#">Huren</a>
+				</div>
+			</div>
+			<div id="contenttext">
+				<div style="padding:10px">
+					<span class="titletext">Welkom bij de site van chiro Kasterlee</span>
+					</div>
+					<div class="bodytext" style="padding:12px;" align="justify">
+					<strong>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur id est tincidunt nisl pellentesque tincidunt. Donec in mauris. Mauris neque magna, consectetuer id, malesuada vitae, tincidunt sit amet, mi. Aliquam lacinia. Suspendisse potenti. Proin justo lorem, rutrum ac, facilisis in, malesuada sed, ligula. </strong><br />
+					<br />
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur id est   tincidunt nisl pellentesque tincidunt. Donec in mauris. Mauris neque magna,   consectetuer id, malesuada vitae, tincidunt sit amet, mi. Aliquam lacinia.   Suspendisse potenti. Proin justo lorem, rutrum ac, facilisis in, malesuada sed,   ligula. Mauris lobortis lacus at nibh. Aenean vitae odio vel odio placerat   hendrerit. Suspendisse lacus lacus, tempor id, pharetra eget, ornare sit amet,   pede. Sed aliquet, justo ac elementum pretium, arcu leo placerat est, a luctus   purus diam eget arcu. Nam augue diam, mollis a, scelerisque eget, aliquet   condimentum, pede. Vestibulum tristique lectus sed augue.<br /><br />
+					Aenean ut mauris luctus mauris interdum convallis. Nunc vestibulum sodales nulla. Nulla vitae massa. Maecenas vel tellus vitae elit mattis adipiscing. In pulvinar felis sed est. Mauris non mi. Duis ultrices dolor ut orci. Quisque lacinia arcu et purus. Sed euismod metus nec augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc dolor leo, aliquam a, placerat sit amet, accumsan eget, dolor. Sed lacinia augue in magna. Fusce sed enim. Vestibulum et mauris. Phasellus in lectus. Pellentesque eu elit in dolor ullamcorper sodales. Vestibulum interdum ornare ligula. Mauris felis odio, rhoncus sed, adipiscing fermentum, tincidunt eu, metus. Suspendisse viverra rhoncus purus.
+				</div>
+			</div>
+			<div id="leftpanel">
+				<div align="justify" class="graypanel">
+					<span class="smalltitle">Nieuws</span><br /><br />
+					<span class="smallredtext">10 september 2012</span><br />
+					<span class="bodytext">Nieuwe programma's toegevoegd.</span><br />
+					<a href="#" class="smallgraytext">Bekijk deze van de jongens</a><br />
+					<a href="#" class="smallgraytext">Bekijk deze van de meisjes</a><br /><br />
+					<span class="smallredtext">16 augustus 2012</span><br />
+					<span class="bodytext">Kampfoto's jongens toegevoegd</span><br />
+					<a href="#" class="smallgraytext">Bekijk</a><br /><br />
+					<span class="smallredtext">2 augustus 2012</span><br />
+					<span class="bodytext">Kampfoto's meisjes toegevoegd</span><br />
+					<a href="#" class="smallgraytext">Bekijk</a><br /><br />
+				</div>
+			</div>
+			<div id="footer" class="smallgraytext">
+				<a href="#">Home</a> | <a href="#">Contacteer ons</a>
+				| chiro Kasterlee
+				&copy; 2012 | <a href="http://www.winkhosting.com" target="_blank"></a> 
+			</div>
+		</div>
+	</div>
+</body>
+</html>
