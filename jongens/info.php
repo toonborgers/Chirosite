def0df745630606845b38ac86553cb30afcc98b7
Roger Vandewalle <stijn_hoskens2@hotmail.com>
2012-10-02 00:13:59 +0200

    de kaderkes werke, nei nog late werke me database

--- jongens/info.php
+++ jongens/info.php
@@ -16,12 +16,22 @@
 					<div style="padding:10px">
 						<span class="titletext">Leiders</span>
 					</div>
-					<div class="paarseachtergrond">
-						test.
-					</div>
-					<div class="paarseachtergrond">
-						test2.
-					</div>
+					<table>
+						<tr>
+							<td>
+								<div class="paarseachtergrond" align="center">
+									<img src="fotosleiders/michieldierckx.jpg" width="200"/><br />
+									<font face="Arial" size="2">Michiel Dierckx <br />
+									<a href="mailto:michiel.dierckx@chirokasterlee.be">michiel.dierckx@chirokasterlee.be</a>
+								</div>
+							</td>
+							<td>
+								<div class="paarseachtergrond">
+									test2.
+								</div>
+							</td>
+						</tr>
+					</table>
 				</div>			
 				<?php include("../footer.html"); ?>
 			</div>
