478df20941fdbf460d0f22c371cf5c7ed94ec124
Roger Vandewalle <stijn_hoskens2@hotmail.com>
2012-10-01 22:59:43 +0200

    schoon kaderkes gemaakt voor foto's leiders & programma's

--- jongens/info.php
+++ jongens/info.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php include "../database/db-info.php";?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
@@ -15,12 +16,12 @@
 					<div style="padding:10px">
 						<span class="titletext">Leiders</span>
 					</div>
-					<?php include("retrieveLeider.php?db='$db'&afdeling='sloebers'"; ?>
-					<?php include("retrieveLeider.php?db='$db'&afdeling='speelclub'"; ?>
-					<?php include("retrieveLeider.php?db='$db'&afdeling='rakkers'"; ?>
-					<?php include("retrieveLeider.php?db='$db'&afdeling='toppers'"; ?>
-					<?php include("retrieveLeider.php?db='$db'&afdeling='kerels'"; ?>
-					<?php include("retrieveLeider.php?db='$db'&afdeling='aspiranten'"; ?>
+					<div class="paarseachtergrond">
+						test.
+					</div>
+					<div class="paarseachtergrond">
+						test2.
+					</div>
 				</div>			
 				<?php include("../footer.html"); ?>
 			</div>
