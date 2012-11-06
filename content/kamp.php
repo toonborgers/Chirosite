6bfab818bd4283508ebe8cb5928a0af06d68018d
Toon Borgers <borgers.toon@gmail.com>
2012-11-06 20:04:45 +0100

    ah un

--- content/kamp.php
+++ content/kamp.php
@@ -5,7 +5,7 @@
 	<?php
 		$chiro = $_GET['chiro'];
 		$queryResult = doSelectForSingleResult("SELECT adres, tekst FROM new_kamp WHERE chiro='$chiro'");
-		if(mysql_num_rows($queryResult)==0) {
+		if(count($queryResult)==0) {
 			echo "Rarara.. naar waar zullen we dit jaar op kamp gaan?!";
 		} else {
 			echo $queryResult['tekst'];
