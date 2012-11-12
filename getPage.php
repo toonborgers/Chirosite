bd1c4d87d7341d988d9c36d8e25753de71ab4d27
Toon Borgers <borgers.toon@gmail.com>
2012-11-12 21:35:30 +0100

    tjirp

--- getPage.php
+++ getPage.php
@@ -5,15 +5,23 @@
 
 		//hier nieuwe paginas zetten, dan op de link (in menu bvb): <a href="index.php?page=bla"/>
 		$pages = array(
-			"home" => "home.php",
-			"info" => "info.php",
-			"prog" => "prog.php",
-			"jHuren" => "jHuren.php",
-			"kamp" => "kamp.php",
-			"portal" => "portal.php",
-			"contact" => "contact.php"
+			"home" => array("home.php", false),
+			"info" => array("info.php", false),
+			"prog" => array("prog.php", false),
+			"jHuren" => array("jHuren.php", false),
+			"kamp" => array("kamp.php", false),
+			"portal" => array("portal.php", true),
+			"contact" => array("contact.php", false)
 		);
 		
-		$page = array_key_exists ($pageName, $pages) ? $pages[$pageName] : $pages["home"] ;
+		$page = array_key_exists ($pageName, $pages) ? $pages[$pageName][0] : "home.php";
+		
+		if(array_key_exists ($pageName, $pages)){
+			$page = $pages[$pageName][0];
+			
+			if($pages[$pageName][0] && !isset($_SESSION['login'])){
+				$page = "home.php";
+			}
+		}
 	}
 ?>
