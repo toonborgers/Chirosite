2bc6ed85d2d26855dc16b339a0c2e2886d483fb0
Stijn <stijn_hoskens2@hotmail.com>
2013-03-22 17:35:43 +0100

    kalender sorteert nu fatsoenlijk

--- content/kalender.php
+++ content/kalender.php
@@ -4,13 +4,14 @@
 <div class="bodytext">
 	<?php
 	$chiro = $_GET['chiro'];
-	$kalenderItems = doSelectForMultipleResults("SELECT DISTINCT id, date_format(datum, '%d/%c/%Y') datum, activiteit FROM new_kalender WHERE chiro='$chiro' OR chiro='b' ORDER BY datum ASC");
+	$sql = "SELECT DISTINCT id, datum, activiteit FROM new_kalender WHERE chiro='$chiro' OR chiro='b' ORDER BY datum ASC";
+	$kalenderItems = doSelectForMultipleResults($sql);
 	if (empty($kalenderItems)) {
 		$kalenderTabel = "Er zijn geen kalender-items om weer te geven.";
 	}
 	echo "<table cellspacing='10'>";
 	foreach ($kalenderItems as $kalenderItem) {
-		echo "<tr><td><b>" . $kalenderItem['datum'] . "</b></td><td>" . $kalenderItem['activiteit'] . "</td></tr>";
+		echo "<tr><td><b>" . date("d/m/Y", strtotime($kalenderItem['datum'])) . "</b></td><td>" . $kalenderItem['activiteit'] . "</td></tr>";
 	}
 	echo "</table>";
 ?>
