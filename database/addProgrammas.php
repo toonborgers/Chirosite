7d036ad82c0fd972557bcb59b51f4ce335f72b9d
Stijn <stijn_hoskens2@hotmail.com>
2013-04-30 01:53:28 +0200

    programma's aangepast, moet nu werken

--- database/addProgrammas.php
+++ database/addProgrammas.php
@@ -6,7 +6,7 @@
 	for ($i=0; $i < 7; $i++) { 
 		$programma = $_POST["$i"];
 		$programma = str_replace("'", "''", $programma);
-		delete("DELETE FROM new_programmas WHERE groep='$i';");
+		delete("DELETE FROM new_programmas WHERE groep='$i' AND chiro='$chiro';");
 		doInsert("INSERT INTO new_programmas VALUES (NULL , '$chiro' , '$sqlDate' , '$i' , '$programma');");
 	}
 	header("Location: ../index.php?page=portal");
